572a3b11899b07c80a27e92d5f226c8a7a68ce5c
Mohed <Mohed@hotmail.com>
2017-10-21 14:02:35 +0200

    Initial spring boot project structure

---
+++ src/main/java/com/unsocialmedia/intouch/IntouchApplication.java
@@ -0,0 +1,12 @@
+package com.unsocialmedia.intouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntouchApplication.class, args);
+	}
+}
